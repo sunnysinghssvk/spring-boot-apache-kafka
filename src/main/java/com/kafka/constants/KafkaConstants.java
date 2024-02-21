7e7acf09be9d68aad111d16c48f3b10872889368
Sunny <sunnysinghssvk@gmail.com>
2024-02-21 16:35:34 +0530

    Added Constants class

---
+++ src/main/java/com/kafka/constants/KafkaConstants.java
@@ -0,0 +1,10 @@
+package com.kafka.constants;
+
+public class KafkaConstants {
+    public static final String EMPLOYEE_NAME = "employeeName";
+    public static final String EMPLOYEE_AGE = "employeeAge";
+    public static final String EMPLOYEE_CITY = "employeeCity";
+    public static final String EMPLOYEE_EMAIL = "employeeEmail";
+    public static final String EMPLOYEE_MOBILE_NO = "employeeMobileNo";
+    public static final String EMPLOYEE_IS_FULLSTACK_DEVELOPER = "employeeIsFullStackDeveloper";
+}
